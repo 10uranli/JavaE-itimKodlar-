f1392d76225d309f8826205749bfc04d1ce02a02
10ur <10ur@DESKTOP-1SOALIN>
2016-07-13 18:31:02 +0300

    Degsiskenler

---
+++ JavaDersKodları/src/JavaGirisKodları/Degiskenler.java
@@ -0,0 +1,16 @@
+package JavaGirisKodlarý;
+
+public class Degiskenler {
+
+	public static void main(String[] args) {
+		int in= 1;
+		long lo = 1919;
+		String Sehir = "Kars";
+		double doub = 1.0;
+		double x  = in+lo+doub;
+		System.out.println(Sehir+"'ýn kurtuluþu: "+x);
+		
+
+	}
+
+}
